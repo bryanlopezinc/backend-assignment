8ac7f567fedccf0041623a970e832edf3efbe9e8
BRYAN INC <bryanlopez2kul2@yahoo.com>
2022-03-10 01:40:11 +0100

    Added log requests feature

---
+++ app/Http/Middleware/RequestLoggerMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
+
+final class RequestLoggerMiddleware
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->logger->info(json_encode([
+            'method'  => $request->getMethod(),
+            'uri'     => $request->getPathInfo(),
+            'body'    => $request->all(),
+            'headers' => $request->headers->all(),
+        ]), [
+            'request_id' => Uuid::uuid4()->toString()
+        ]);
+
+        return $next($request);
+    }
+}
